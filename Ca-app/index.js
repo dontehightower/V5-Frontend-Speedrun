91ea2d99fb370c842778da312a9327c12c5d583b
dontehightower <dontedhightower@gmail.com>
2018-05-25 01:16:11 -0500

    add testimonials, monthly membership, team,  and contact form

--- Ca-app/index.js
+++ Ca-app/index.js
@@ -1,10 +1,10 @@
-function scrollFunction() {
+const scrollFunction = () => {
   const header = document.querySelector('header');
-  if (window.scrollY > 48) {
+  if (window.scrollY > 0) {
     // header.classList.remove('initial-header');
     header.classList.add('fixed-header');
   } else {
     header.classList.remove('fixed-header');
     // header.classList.add('initial-header');
   }
-}
+};
